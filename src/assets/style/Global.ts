4384362dc27dc8be7bf95eb6d5dfcb007e55b7c9
untlsn <untellson@gmail.com>
2021-03-25 18:25:58 +0100

    Add react-helmet primary for add fonts

--- src/assets/style/Global.ts
+++ src/assets/style/Global.ts
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const robotoUrl = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap';
-
 const GlobalStyle = createGlobalStyle`
-  @import url(${robotoUrl});
-  
   *, *::after, *::before {
     box-sizing: border-box;
   }
--- src/providers/Style.tsx
+++ src/providers/Style.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from '@style/all';
+import { Helmet } from 'react-helmet';
 
 const StyleProvider = ({ children }: { children: any }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </>
   );
 };
 
