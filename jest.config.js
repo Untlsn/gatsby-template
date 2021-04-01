92a9fb9ec499b59843cc9750e718373715e55102
untlsn <untellson@gmail.com>
2021-04-01 11:14:48 +0200

    Add simple configs

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$',
+  moduleNameMapper: require('./jest-alias'),
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+  setupFiles: ['<rootDir>/loadershim.js'],
+};
